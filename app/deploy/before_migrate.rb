c032edab9023e839bbd0502334afe28de9391922
shokuan <contact.shokuan@gmail.com>
2018-02-10 23:34:03 +0900

    test

--- app/deploy/before_migrate.rb
+++ app/deploy/before_migrate.rb
@@ -1,8 +1,8 @@
-env = node[:deploy][:sample_rails_app][:rails_env]
-current_release = release_path
+# env = node[:deploy][:sample_rails_app][:rails_env]
+# current_release = release_path
 
-execute "rake assets:precompile" do
-  cwd current_release
-  command "bundle exec rake assets:precompile"
-  environment "RAILS_ENV" => env
-end
+# execute "rake assets:precompile" do
+#   cwd current_release
+#   command "bundle exec rake assets:precompile"
+#   environment "RAILS_ENV" => env
+# end
