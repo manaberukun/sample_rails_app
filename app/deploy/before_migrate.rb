b308692701d91da225fcc11bf41092c7b1419828
shokuan <contact.shokuan@gmail.com>
2018-02-10 23:54:08 +0900

    chage dir

--- app/deploy/before_migrate.rb
+++
@@ -1,8 +0,0 @@
-# env = node[:deploy][:sample_rails_app][:rails_env]
-# current_release = release_path
-
-# execute "rake assets:precompile" do
-#   cwd current_release
-#   command "bundle exec rake assets:precompile"
-#   environment "RAILS_ENV" => env
-# end
